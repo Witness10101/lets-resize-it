b15503f7949974a7d99d939dfb36a31fe4d4b4af
Witness10101 <130312128+Witness10101@users.noreply.github.com>
2026-09-01 22:41:05 +0530

    Add GA4 tracking for Let's Resize It

---
+++ components/analytics/GoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Script from "next/script";
+
+const GA_MEASUREMENT_ID = "G-Y783TPGYVQ";
+
+export function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          window.gtag = gtag;
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+    </>
+  );
+}
